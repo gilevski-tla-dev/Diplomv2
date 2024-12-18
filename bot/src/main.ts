8c104ea2d00e8f6299c050e8a742b11545f04d9a
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-18 19:22:29 +0200

    init

---
+++ bot/src/main.ts
@@ -0,0 +1,25 @@
+import { Telegraf } from "telegraf";
+
+// Создайте экземпляр бота с токеном
+const bot = new Telegraf("5821534037:AAGGxevje9-Qlf4PSh6TeIjPWFiEWzrrXC0");
+
+// Обработчик команды /start
+bot.command("start", (ctx) => {
+  ctx.reply("Привет! Нажми на кнопку ниже, чтобы открыть Web App:", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Открыть Web App", // Текст на кнопке
+            web_app: { url: "https://your-webapp-url.com" }, // URL вашего веб-приложения
+          },
+        ],
+      ],
+    },
+  });
+});
+
+// Запуск бота
+bot.launch();
+
+console.log("Бот запущен!");
